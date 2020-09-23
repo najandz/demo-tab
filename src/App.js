fe045211d9b921fecf248292fb31fec2f05cd8be
Zareei, Nazhand <Nazhand.Zareei@arcteryx.com>
2020-09-23 14:44:23 -0700

    adding tabs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tabs from "./components/Tabs";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="content">
+			<Tabs>
+				<div label="My Details">Nazhand Zareei!</div>
+				<div label="My Order">List of My Order!</div>
+				<div label="My Returns">List of My Return!</div>
+			</Tabs>
+		</div>
+	);
 }
 
 export default App;
